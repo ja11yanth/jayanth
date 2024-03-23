f91555030da0915ffc37dd228c7f58e1c88a849b
unknown <babunbabu1108@gmail.com>
2024-03-24 03:00:36 +0530

    update script.sh

--- script.sh
+++ script.sh
@@ -5,3 +5,8 @@ sudo yum install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo yum update httpd
+
